94b6866f0c71f1ed543c88360fa1e688253d56a7
msmith89-wq <solid@Wraithcom>
2025-02-06 20:30:24 -0600

    added data table to tab

--- Meteorite_Fireball_App/server.R
+++ Meteorite_Fireball_App/server.R
@@ -7,18 +7,29 @@ library(shiny)
 # Define server logic required to draw a histogram
 
 function(input, output, session){ 
-  output$radiatePlot <- renderPlot({
+  output$radiatePlot <- renderPlotly({
     
-    fireball_bolides |> 
-      ggplot(aes(x = log(Total_Radiated_Energy_J), y = Altitude_km)) + 
+    plot_2 <- fireball_bolides |> 
+      ggplot(aes(x = log(Total_Radiated_Energy_J), y = Altitude_km, text = paste(log(Total_Radiated_Energy_J), Altitude_km))) + 
       geom_point() 
+    
+    ggplotly(plot_2, tooltip = 'text')
   })
   
-  output$impactPlot <- renderPlot({
+  output$impactPlot <- renderPlotly({
     
-    fireball_bolides |> 
-      ggplot(aes(x = log(Total_Impact_Energy_kt), y = Altitude_km)) + 
+    plot <- fireball_bolides |> 
+      ggplot(aes(x = log(Total_Impact_Energy_kt), y = Altitude_km, text = paste(log(Total_Impact_Energy_kt), Altitude_km))) + 
       geom_point()
+    
+    ggplotly(plot, tooltip='text')
   })
+  
+  output$Landings_Data <- renderDataTable(
+    datatable(
+      meteorite_landings, 
+      options = list(pageLength = 5)
+    )
+  )
   
 }  
--- Meteorite_Fireball_App/ui.R
+++ Meteorite_Fireball_App/ui.R
@@ -6,35 +6,48 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 fluidPage(
-  
-  # Application title
-  titlePanel("Meteorites Data"),
-  
-  # Sidebar with a slider input for number of bins
-  #sidebarLayout(
-  #sidebarPanel(
-  #selectInput("histvar",
-  #"Select variable for histogram:",
-  #choices = numeric_vars),
-  
-  #sliderInput("bins",
-  #"Number of bins:",
-  # min = 1,
-  # max = 50,
-  # value = 30),
-  
-  #selectInput("island", label = "Select Island:",
-  #choices = c("All", penguins |> distinct(island) |> pull(island) |> sort())),
-  #width = 3
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    fluidRow(
-      column(width = 6,
-             plotOutput("radiatePlot", height=plot_heights)
-      ),
-      column(width = 6,
-             plotOutput("impactPlot", height=plot_heights)
+  tabsetPanel(
+    tabPanel("Radiant Energy and Impact Energy vs Altitude",
+             
+             # Application title
+             titlePanel("Meteorites Data"),
+             
+             # Sidebar with a slider input for number of bins
+             #sidebarLayout(
+             #sidebarPanel(
+             #selectInput("histvar",
+             #"Select variable for histogram:",
+             #choices = numeric_vars),
+             
+             #sliderInput("bins",
+             #"Number of bins:",
+             # min = 1,
+             # max = 50,
+             # value = 30),
+             
+             #selectInput("island", label = "Select Island:",
+             #choices = c("All", penguins |> distinct(island) |> pull(island) |> sort())),
+             #width = 3
+             
+             # Show a plot of the generated distribution
+             mainPanel(
+               fluidRow(
+                 column(width = 6,
+                        plotlyOutput("radiatePlot", height=plot_heights)
+                 ),
+                 column(width = 6,
+                        plotlyOutput("impactPlot", height=plot_heights)
+                 )
+               )
+             )
+    ),
+    
+    tabPanel(
+      "Datasets",
+      fluidRow(
+        column(width = 12,
+               dataTableOutput("Landings_Data")
+        )
       )
     )
     #fluidRow(
@@ -43,6 +56,6 @@ fluidPage(
     #)
   )
 )
-  
+
 
 
